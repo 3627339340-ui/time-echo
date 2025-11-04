90334c2f284108889b4362dbf193c8c9d1dd23c4
wangyidan <3627339340@qq.com>
2025-11-04 22:14:44 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "future-echo",
+  "version": "1.0.0",
+  "description": "未来回音 - 智谱AI信件生成器",
+  "main": "index.html",
+  "scripts": {
+    "start": "node api/generate.js"
+  },
+  "dependencies": {
+    "axios": "^1.6.0"
+  }
+}
